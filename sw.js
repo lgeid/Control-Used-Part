01b281e0c0700f6447ed36535e25ed2e8a34ed05
Suryadi <42304142+lgeid@users.noreply.github.com>
2026-09-01 22:37:18 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,30 @@
-const CACHE_NAME = 'sparepart-lg-v1';
-const ASSETS = [
-  '/Control-Used-Part/',
-  '/Control-Used-Part/index.html'
+const CACHE_NAME = 'lg-parts-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://files.catbox.moe/07qfx0.jpeg'
 ];
 
+// Install Service Worker & Cache
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(names => 
-      Promise.all(names.filter(n => n !== CACHE_NAME).map(c => caches.delete(c)))
-    )
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
   );
 });
 
+// Fetch Assets from Cache if Offline
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
   );
 });
